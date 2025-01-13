cfa60012ca2f96875756a5740d2900b7a8043229
YuYeaJin <yyj9290@naver.com>
2025-01-13 13:06:23 +0900

    Spring 시작

---
+++ src/main/java/himedia/hellospring/controllers/HelloConeroller.java
@@ -0,0 +1,21 @@
+package himedia.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+//	POJO
+@Controller
+public class HelloConeroller {
+	// 메서드가 /hello 요청에 반응할 수 있도록 URL Mapper 등록
+	@RequestMapping("/hello")
+	public ModelAndView hello(@RequestParam("name") String name) {
+		// ModeAndview : 모델과 view 정보를 같이 가지고 있는 객체
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("massage", "Hello, " + name);		// 모델 설정
+		mav.setViewName("/WEB-INF/views/hello.jsp");	// 뷰 정보 설정
+		
+		return mav;
+	}
+}
